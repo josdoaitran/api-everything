fe94db820040ec7928c94fc814c6656588625ffc
doaitran <josdoaitran@gmail.com>
2019-06-03 11:49:28 +0700

    booker request

--- simpleRequest/bookerRequest.py
+++ simpleRequest/bookerRequest.py
@@ -2,6 +2,7 @@ import requests
 
 def booking_request(booking_id):
 
-return requests.get('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id))
+    return requests.get('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id))
 
+print (booking_request(10))
 
