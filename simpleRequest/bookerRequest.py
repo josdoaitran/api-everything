6ea38fe095ed0bf5c9c1411323ebb77d175f9024
doaitran <josdoaitran@gmail.com>
2019-06-03 14:28:56 +0700

    update simeple

--- simpleRequest/bookerRequest.py
+++ simpleRequest/bookerRequest.py
@@ -1,12 +1,24 @@
 import requests
 
-def booking_request(booking_id):
 
+def booking_request(booking_id):
     return requests.get('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id))
+
 
 def get_booking_request(booking_name):
-
     return requests.get('https://restful-booker.herokuapp.com/booking/{:d}/'.format(booking_id))
 
-print (booking_request(10).content)
 
+response = booking_request(10)
+
+print('content of response')
+
+print(response.content)
+
+print('Header of response')
+
+print(response.headers)
+
+print('Status Code of response')
+
+print(response.status_code)
